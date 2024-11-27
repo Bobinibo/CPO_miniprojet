fc9cd4b79d004eaf6e99ff3a0f753b153c9426ef
lilou <lilou@10.3.60.60>
2024-11-27 14:50:27 +0100

    commit 3

--- MiniProjet/src/miniprojet/MiniProjet.java
+++ MiniProjet/src/miniprojet/MiniProjet.java
@@ -11,25 +11,22 @@ import java.util.ArrayList;
 
 public class MiniProjet {
     public static void main(String[] args) {
-        // Couleurs disponibles pour les pions
+      
         ArrayList<Character> couleursDisponibles = new ArrayList<>();
-        couleursDisponibles.add('R'); // Rouge
-        couleursDisponibles.add('B'); // Bleu
-        couleursDisponibles.add('G'); // Vert
-        couleursDisponibles.add('Y'); // Jaune
-        couleursDisponibles.add('O'); // Orange
-        couleursDisponibles.add('P'); // Violet
+        couleursDisponibles.add('R'); 
+        couleursDisponibles.add('B'); 
+        couleursDisponibles.add('G'); 
+        couleursDisponibles.add('Y'); 
+        couleursDisponibles.add('O'); 
+        couleursDisponibles.add('P'); 
 
-        // Génération d'une combinaison aléatoire
         Combinaison combinaisonSecrete = Combinaison.genererAleatoire(4, couleursDisponibles);
         System.out.println("Combinaison secrète générée : " + combinaisonSecrete);
 
-        // Création d'une combinaison proposée
         Pion[] pionsProposes = {new Pion('R'), new Pion('B'), new Pion('G'), new Pion('Y')};
         Combinaison combinaisonProposee = new Combinaison(pionsProposes);
         System.out.println("Combinaison proposée : " + combinaisonProposee);
 
-        // Comparaison des combinaisons
         int[] resultats = combinaisonSecrete.comparer(combinaisonProposee);
         System.out.println("Bien placés (noirs) : " + resultats[0]);
         System.out.println("Mal placés (blancs) : " + resultats[1]);
