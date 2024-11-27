c42bf59d8afe18321aaaf3e806bcaeb4456f3930
lilou <lilou@10.3.60.60>
2024-11-27 10:23:16 +0100

    Test

--- MiniProjet/src/miniprojet/MiniProjet.java
+++ MiniProjet/src/miniprojet/MiniProjet.java
@@ -14,7 +14,7 @@ public class MiniProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println (Maths.abs(a*b))
+
     }
     
 }
