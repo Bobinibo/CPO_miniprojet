e988637dcc20f153421e4f863d38852f82b92a60
Alois Fernandez <aloisfernandezbernier@gmail.com>
2024-11-27 10:12:35 +0100

    MiniProjet

--- MiniProjet/src/miniprojet/MiniProjet.java
+++ MiniProjet/src/miniprojet/MiniProjet.java
@@ -14,7 +14,7 @@ public class MiniProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+System.out.println (Maths.abs(a*b))
     }
     
 }
