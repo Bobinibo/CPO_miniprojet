bde4794c8058cd7084d3e44a5625cc9ab0fc24a9
Alois Fernandez <aloisfernandezbernier@gmail.com>
2024-11-27 11:00:36 +0100

    MiniProjet

--- MiniProjet/src/miniprojet/MiniProjet.java
+++ MiniProjet/src/miniprojet/MiniProjet.java
@@ -9,12 +9,24 @@ package miniprojet;
  * @author alois
  */
 public class MiniProjet {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-System.out.println (Maths.abs(a*b))
+        Pion pionRouge = new Pion(Pion.ROUGE);
+        Pion pionBleu = new Pion(Pion.BLEU);
+        Pion pionVert = new Pion(Pion.VERT);
+        Pion pionJaune = new Pion(Pion.JAUNE);
+        Pion pionOrange = new Pion(Pion.ORANGE);
+        Pion pionMarron = new Pion(Pion.MARRON);
+        Pion pionBlanc = new Pion(Pion.BLANC);
+        Pion pionNoir = new Pion(Pion.NOIR);
+
+        System.out.println("Pion Rouge : " + pionRouge);
+        System.out.println("Pion Bleu : " + pionBleu);
+        System.out.println("Pion Vert : " + pionVert);
+        System.out.println("Pion Jaune : " + pionJaune);
+        System.out.println("Pion Orange : " + pionOrange);
+        System.out.println("Pion Marron : " + pionMarron);
+        System.out.println("Pion Blanc : " + pionBlanc);
+        System.out.println("Pion Noir : " + pionNoir);
     }
-    
 }
+
